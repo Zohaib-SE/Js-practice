b1dee87ae46dd1013f19b5d952388a4daef4890d
Zohaib Ali Mirza <zohaibliaqat914@gmail.com>
2024-08-28 04:38:12 -0700

    Date and Time in Js

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,30 @@
+// Dates
+
+let myDate = new Date()
+
+// console.log(myDate);
+// Expected Output: 2024-08-21T00:55:51.582Z
+
+// console.log(myDate.toString()); 
+// Expected Output:Tue Aug 20 2024 17:55:51 GMT-0700 (Pacific Daylight Time)
+
+// console.log(myDate.toISOString());
+// Expected Output:2024-08-21T00:55:51.582Z
+
+// console.log(myDate.toJSON()); 
+// Expected Output: 2024-08-21T00:55:51.582Z
+
+// console.log(myDate.toLocaleDateString());
+// Expected Output:8/20/2024
+
+// console.log(myDate.toLocaleString());
+// Expected Output: 8/20/2024, 5:55:51 PM
+
+// console.log(myDate.toTimeString());
+// Expected Output: 17:55:51 GMT-0700 (Pacific Daylight Time)
+
+// console.log(myDate.toUTCString());
+// Expected Output: Wed, 21 Aug 2024 00:55:51 GMT
+
+console.log(myDate.getTimezoneOffset());
+
