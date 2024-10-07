9cfb8cd130bc40e11e05fb4a96410eb85898aefb
Zohaib-SE <zohaibliaqat914@gmail.com>
2024-10-07 17:01:07 +0500

    Js fundamentals

--- 02_basics_js/04_object.js
+++ 02_basics_js/04_object.js
@@ -29,7 +29,7 @@ const obj3 = {5: "a" , 6: "b"}
 // const obj4 = Object.assign({}, obj1, obj2, obj3)
 
 const obj4 = {...obj1, ...obj2, ...obj3}
-console.log(obj4);
+// console.log(obj4);
 
 
 const users = [
@@ -47,11 +47,39 @@ const users = [
     },
 ]
 
-console.log(users[1].email)
+// console.log(users[1].email)
 // console.log(tinderUser);
 
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+//DE-STRUCTURING
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Zohaib",
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course // de-structuring of object
+
+console.log(instructor);
+// Expected result: Zohaib
+
+// {
+//     "name": "Zohaib",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// } // Json
+
+[
+    {},
+    {},
+    {},
+]
